1b2c9243ec92f92bba06a9d34266b8f60d832d30
Jassi Chahal <jassikkchahal@gmail.com>
2023-10-02 11:19:57 -0500

    initial commit to main

--- app/src/main/java/com/cs407/lab3_milestone2/MainActivity.java
+++ app/src/main/java/com/cs407/lab3_milestone2/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //initial
+    //initial - main
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
